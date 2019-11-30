5eae279ee85d72b5376dbb51e05fd81b03a325fb
Marc Brout <marc.brout@epitech.eu>
2019-11-30 16:18:08 +0100

    added & tested argument parsing

---
+++ src/main/java/parsing/CommandLine.java
@@ -0,0 +1,96 @@
+package parsing;
+
+import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class CommandLine {
+    private static final int OPTION_NOT_FOUND = -1;
+    private boolean parsedOnce = false;
+
+    /**
+     * These options are used to parse the arguments passed to the parse() method.
+     */
+    private ArrayList<Option> options = new ArrayList<>();
+
+    public void addOption(Option o) {
+        options.add(o);
+    }
+
+    private int findFirstOption(final Option option, final String[] inputs) {
+        int i = 0;
+        String opt = "-" + option.getOpt();
+        String longOpt = "-" + option.getLongOpt();
+
+        while (i < inputs.length) {
+            if (inputs[i].equals(opt) || inputs[i].equals(longOpt)) {
+                return i;
+            }
+            ++i;
+        }
+        return OPTION_NOT_FOUND;
+    }
+
+    /**
+     * This method parse arguments passed to the program in the main function.
+     * It translates the command line arguments directly to a readable map containing their values.
+     *
+     * @param argumentsToParse are the one received directly by the program.
+     */
+    public void evaluate(final String[] argumentsToParse) {
+        for (Option option : this.options) {
+            int pos = findFirstOption(option, argumentsToParse);
+            if (isInputValid(argumentsToParse, pos)) {
+                option.setValue(argumentsToParse[pos + 1]);
+            } else if (option.isRequired()) {
+                throw new IllegalArgumentException("Argument : -" + option.getOpt() + " is missing its value!");
+            }
+        }
+    }
+
+    private boolean isInputValid(String[] argumentsToParse, int pos) {
+        return pos != OPTION_NOT_FOUND && pos + 1 < argumentsToParse.length && argumentsToParse[pos + 1].charAt(0) != '-';
+    }
+
+    public boolean isValid() {
+        AtomicBoolean allOptionsAreValid = new AtomicBoolean(true);
+        options.forEach((option -> {
+            if (!option.isValid()) {
+               allOptionsAreValid.set(false);
+            }
+        }));
+        return allOptionsAreValid.get();
+    }
+
+    private Option getOption(String opt) {
+        int i = 0;
+
+        while (i < options.size()) {
+            Option currentOption = options.get(i);
+            if (currentOption.getOpt().equals(opt)) {
+                return currentOption;
+            }
+            ++i;
+        }
+        return null;
+    }
+
+    public String getOptionValue(String opt) {
+        Option option = getOption(opt);
+
+        if (option == null || option.getValue() == null) {
+            throw new NullPointerException("Call method hasOption() before trying to access options values");
+        }
+
+        return option.getValue();
+    }
+
+    public boolean hasOptions(String opt) {
+        Option option = getOption(opt);
+        return option != null && option.isValid() && option.getValue() != null;
+    }
+
+    public int size() {
+        return options.size();
+    }
+}
+
---
+++ src/test/java/parsing/OptionTest.java
@@ -0,0 +1,84 @@
+package parsing;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class OptionTest {
+    Option requiredOption;
+    Option optionalOption;
+
+    @BeforeEach
+    void prepare() {
+        requiredOption = new Option.Builder("p")
+                .setLongOpt("pseudo")
+                .setRequired(true)
+                .setDescription("This is the pseudonym that will be displayed to other users")
+                .build();
+
+        optionalOption = new Option.Builder("o")
+                .setLongOpt("optional")
+                .setRequired(false)
+                .setDescription("Optional Option")
+                .build();
+    }
+
+    @AfterEach
+    void reset() {
+        requiredOption = null;
+        optionalOption = null;
+    }
+
+    @Test
+    void getOpt() {
+        Assertions.assertEquals("p", requiredOption.getOpt());
+        Assertions.assertEquals("o", optionalOption.getOpt());
+    }
+
+    @Test
+    void getLongOpt() {
+        Assertions.assertEquals("pseudo", requiredOption.getLongOpt());
+        Assertions.assertEquals("optional", optionalOption.getLongOpt());
+    }
+
+    @Test
+    void getDescription() {
+        Assertions.assertEquals("This is the pseudonym that will be displayed to other users", requiredOption.getDescription());
+        Assertions.assertEquals("Optional Option", optionalOption.getDescription());
+    }
+
+    @Test
+    void isRequired() {
+        Assertions.assertTrue(requiredOption.isRequired());
+        Assertions.assertFalse(optionalOption.isRequired());
+    }
+
+    @Test
+    void setValue() {
+        Assertions.assertNull(requiredOption.getValue());
+        Assertions.assertNull(optionalOption.getValue());
+
+        requiredOption.setValue("RoxorDu40");
+        optionalOption.setValue("option");
+
+        Assertions.assertEquals("RoxorDu40", requiredOption.getValue());
+        Assertions.assertEquals("option", optionalOption.getValue());
+    }
+
+    @Test
+    void getValue() {
+        setValue();
+    }
+
+    @Test
+    void isValid() {
+        Assertions.assertFalse(requiredOption.isValid());
+        Assertions.assertTrue(optionalOption.isValid());
+
+        requiredOption.setValue("RoxorDu40");
+
+        Assertions.assertTrue(requiredOption.isValid());
+        Assertions.assertTrue(optionalOption.isValid());
+    }
+}
