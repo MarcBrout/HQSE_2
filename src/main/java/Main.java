f5b0daaa2a8705da83802a82545c67f21927e63d
Marc Brout <marc.brout@epitech.eu>
2019-11-30 10:11:20 +0100

    feat: initial java project working

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+public final class Main {
+    /**
+     * Main function.
+     *
+     * @param argv incoming argument from user.
+     */
+    public static void main(final String[] argv) {
+
+    }
+
+    private Main() {
+
+    }
+}
